44948660e1cbcd8728c193c2c61a0d429ac770e5
VimalSurti <vimalsurti01@gmail.com>
2020-03-13 14:31:08 +0100

    Ajout de l'exercice5

--- exercice2/index.js
+++ exercice2/index.js
@@ -50,3 +50,41 @@ function exo2() {
     }
 }
 console.log(exo2());
+
+/*EXERCICE 03
+
+Rendu : ex03.js
+
+Écrivez en Javascript :
+
+- J’initialise a à 8  
+
+- Je stocke a + 1 dans b  
+
+- J’ajoute 1 à a  
+
+- Je multiplie a par 2  
+
+- Je retranche 5 à b  
+
+- Je multiplie b par lui-même 
+
+- J’ajoute 3 à a 
+
+- Si a vaut 1, je lui ajoute 2, sinon je lui ajoute 3  */
+
+    function exo3() {
+        let a = 8
+        let b = a + 1
+        a += 1;
+        a *= 2;
+        b -= 5;
+        b *= b;
+        a += 3;
+        if (a === 1) {
+            return a += 2;
+        } else {
+            return a += 3;
+        }
+    }
+    console.log(exo3());
